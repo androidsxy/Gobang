4d4f933551f62b525c5d7f30c50b0ba9efea7976
androidsxy <1274187367@qq.com>
2018-12-04 17:26:14 +0800

    完成基本功能;

--- js/index.js
+++ js/index.js
@@ -3,7 +3,12 @@ var td = document.getElementsByClassName("td");
 var start = document.getElementById("start");
 var tiem = document.getElementById("tiem");
 var helpbottom = document.getElementById("helpbottom");
+var help = document.getElementById("help");
 var both_sides = document.getElementById("both_sides");
+var popup_content = document.getElementById("popup_content");
+var popup = document.getElementById("popup");
+var button = document.getElementById("button");
+var audio = document.getElementById("audio");
 var isWhitebole = false;
 //保存一下棋子
 var pieceList = [];
@@ -15,10 +20,14 @@ var whitepiece = [];
 var blackepiece = [];
 //是否结束游戏
 var s = 0;
+var cont = 0;
+var temts = 20;
+var tmem;
 
 function Test() {}
 //生成棋子
 Test.prototype.piece = function(a, event) {
+        audio.play();
         var setX = event.layerX;
         var setH = event.layerY;
         var left = Math.round(setX / 26) * 26;
@@ -36,31 +45,27 @@ Test.prototype.piece = function(a, event) {
             piecediv.style.width = "20px";
             piecediv.style.height = "20px";
             piecediv.style.borderRadius = "50%";
-            pieceList.push([left / 26, top / 26]);
             if (isWhitebole) {
                 piecediv.style.backgroundColor = "white";
                 piecediv.setAttribute("class", "piece_white");
                 both_sides.style.color = "black";
                 both_sides.innerHTML = "黑棋走..";
-                whitepiece.push([left / 26, top / 26, "white"]);
+                pieceList.push([left / 26, top / 26, "white"]);
                 isWhitebole = false;
             } else {
                 piecediv.style.backgroundColor = "black";
                 piecediv.setAttribute("class", "piece_black");
                 both_sides.style.color = "white";
                 both_sides.innerHTML = "白棋走..";
-                blackepiece.push([left / 26, top / 26, "black"]);
+                pieceList.push([left / 26, top / 26, "black"]);
                 isWhitebole = true;
             }
             piecediv.onclick = function(event) {
                 alert("此处以有棋子");
                 event.stopPropagation();
             }
-            this.paixu(pieceList);
-            this.paixu(whitepiece);
-            this.paixu(blackepiece);
-            this.game_Over(whitepiece, piecediv);
-            this.game_Over(blackepiece, piecediv);
+            this.game_Over(pieceList, piecediv);
+            console.log(pieceList);
         } else {
             pieceListBole = 0;
             alert("此处以有棋子");
@@ -68,56 +73,203 @@ Test.prototype.piece = function(a, event) {
     }
     //初始化表格 点击事件
 Test.prototype.init = function() {
-        var that = this;
-        var tdW = parseInt(getComputedStyle(content).width);
-        var tdH = parseInt(getComputedStyle(content).height);
-        for (var i = 0; i < td.length; i++) {
-            td[i].style.width = (tdW - 15) / 15 + "px";
-            td[i].style.height = (tdH - 15) / 15 + "px";
-        }
-        start.onclick = function() {};
-        tiem.onclick = function(event) { event.stopPropagation() };
-        helpbottom.onclick = function(event) { event.stopPropagation() };
-        content.onclick = function(event) {
-            that.piece(this, event);
-        };
+    var that = this;
+    var tdW = parseInt(getComputedStyle(content).width);
+    var tdH = parseInt(getComputedStyle(content).height);
+    for (var i = 0; i < td.length; i++) {
+        td[i].style.width = (tdW - 15) / 15 + "px";
+        td[i].style.height = (tdH - 15) / 15 + "px";
     }
-    //游戏是否完成
-Test.prototype.game_Over = function(list, piecediv) {
-    var listStat = [];
-    console.log(list, ">>>>>>>>>>>>>>>>>>>>>>>>>>");
-    for (var i = 0; i < list.length; i++) {
-        listStat.push(list[i]);
-        for (var a = 1; a < list.length - i; a++) {
-            if (list[i][1] == list[i + a][1]) {
-                listStat.push(list[i + a]);
-            } else if (listStat.length >= 5) {
-                this.paixu1(listStat)
-                console.log(listStat);
-                for (var c = 0; c < listStat.length; c++) {
-                    for (var v = 1; v < listStat.length - c; v++) {
-                        if (listStat[c][0] + v == listStat[c + v][0]) {
-                            s++;
-                        } else {
-                            s = 0;
-                        }
-                    }
-                    if (s == 4) {
-                        return;
-                    }
-                }
-                if (s == 4) {
-                    listStat[0][2] == "black" ? alert("黑棋胜利！！！") : alert("白棋胜利！！！");
-                    s = 0;
-                    return;
-                }
-            } else {
-                listStat = [];
-            }
+    start.onclick = function() {
+        popup_content.style.display = "none";
+        popup.style.display = "none";
+        that.setTmen();
+    };
+    tiem.onclick = function(event) { event.stopPropagation() };
+    helpbottom.onclick = function(event) {
+        help.style.display = "block"
+        popup.style.display = "block";
+        event.stopPropagation()
+    };
+    content.onclick = function(event) {
+        that.piece(this, event);
+    };
+    button.onclick = function() {
+        help.style.display = "none"
+        popup.style.display = "none";
+        event.stopPropagation()
+    }
 
+}
+Test.prototype.setTmen = function() {
+        var that = this;
+        temts = 20;
+        tiem.innerHTML = temts;
+        tmem = setInterval(function() {
+            temts--;
+            tiem.innerHTML = temts;
+            if (temts == 0) {
+                if (pieceList.length > 0) {
+                    if (pieceList[pieceList.length - 1][2] == "white") {
+                        alert("白棋获胜");
+                    } else {
+                        alert("黑棋获胜！");
+                    }
+                } else {
+                    alert("白棋获胜！");
+                }
+                temts = 20;
+                tiem.innerHTML = temts;
+                that.colon();
+                clearInterval(tmem);
+            }
+        }, 1000);
+    }
+    //游戏是否完成 左右
+Test.prototype.game_Over = function(list, piecediv) {
+        for (var u = 1; u < 5; u++) {
+            var cont1 = cont;
+            for (var a = 0; a < list.length; a++) {
+                if ((list[list.length - 1][0] + u) == list[a][0] && list[list.length - 1][1] == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                    cont++;
+                }
+            }
+            if (cont1 == cont) {
+                break;
+            }
+        };
+        for (var u = 1; u < 5; u++) {
+            var cont1 = cont;
+            for (var a = 0; a < list.length; a++) {
+                if ((list[list.length - 1][0] - u) == list[a][0] && list[list.length - 1][1] == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                    cont++;
+                }
+            }
+            if (cont1 == cont) {
+                break;
+            }
+        };
+        if (cont >= 4) {
+            if (list[list.length - 1][2] == "white") {
+                alert("白棋获胜！");
+            } else {
+                alert("黑棋获胜！");
+            }
+            this.colon();
+        } else {
+            cont = 0;
+            this.game_Over_lerig(list, piecediv);
         }
     }
-    content.appendChild(piecediv);
+    //上下
+Test.prototype.game_Over_lerig = function(list, piecediv) {
+        for (var u = 1; u < 5; u++) {
+            var cont1 = cont;
+            for (var a = 0; a < list.length; a++) {
+                if ((list[list.length - 1][0]) == list[a][0] && (list[list.length - 1][1] + u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                    cont++;
+                }
+            }
+            if (cont1 == cont) {
+                break;
+            }
+        };
+        for (var u = 1; u < 5; u++) {
+            var cont1 = cont;
+            for (var a = 0; a < list.length; a++) {
+                if (list[list.length - 1][0] == list[a][0] && (list[list.length - 1][1] - u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                    cont++;
+                }
+            }
+            if (cont1 == cont) {
+                break;
+            }
+        };
+        if (cont >= 4) {
+            if (list[list.length - 1][2] == "white") {
+                alert("白棋获胜！");
+            } else {
+                alert("黑棋获胜！");
+            }
+            this.colon();
+        } else {
+            cont = 0;
+            this.game_Over_left_top_rigth_bottom(list, piecediv);
+        }
+    }
+    //左上 右下
+Test.prototype.game_Over_left_top_rigth_bottom = function(list, piecediv) {
+    for (var u = 1; u < 5; u++) {
+        var cont1 = cont;
+        for (var a = 0; a < list.length; a++) {
+            if ((list[list.length - 1][0] + u) == list[a][0] && (list[list.length - 1][1] + u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                cont++;
+            }
+        }
+        if (cont1 == cont) {
+            break;
+        }
+    };
+    for (var u = 1; u < 5; u++) {
+        var cont1 = cont;
+        for (var a = 0; a < list.length; a++) {
+            if ((list[list.length - 1][0] - u) == list[a][0] && (list[list.length - 1][1] - u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                cont++;
+            }
+        }
+        if (cont1 == cont) {
+            break;
+        }
+    };
+    if (cont >= 4) {
+        if (list[list.length - 1][2] == "white") {
+            alert("白棋获胜！");
+        } else {
+            alert("黑棋获胜！");
+        }
+        this.colon();
+    } else {
+        cont = 0;
+        this.game_Over_rigth_top_left_bottom(list, piecediv);
+
+    }
+}
+Test.prototype.game_Over_rigth_top_left_bottom = function(list, piecediv) {
+    for (var u = 1; u < 5; u++) {
+        var cont1 = cont;
+        for (var a = 0; a < list.length; a++) {
+            if ((list[list.length - 1][0] - u) == list[a][0] && (list[list.length - 1][1] + u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                cont++;
+            }
+        }
+        if (cont1 == cont) {
+            break;
+        }
+    };
+    for (var u = 1; u < 5; u++) {
+        var cont1 = cont;
+        for (var a = 0; a < list.length; a++) {
+            if ((list[list.length - 1][0] + u) == list[a][0] && (list[list.length - 1][1] - u) == list[a][1] && list[list.length - 1][2] == list[a][2]) {
+                cont++;
+            }
+        }
+        if (cont1 == cont) {
+            break;
+        }
+    };
+    if (cont >= 4) {
+        if (list[list.length - 1][2] == "white") {
+            alert("白棋获胜！");
+        } else {
+            alert("黑棋获胜！");
+        }
+        this.colon();
+    } else {
+        cont = 0;
+        clearInterval(tmem);
+        this.setTmen();
+        content.appendChild(piecediv);
+    }
 }
 Test.prototype.paixu = function(list) {
     list.sort(function(x, y) {
@@ -142,6 +294,7 @@ Test.prototype.colon = function() {
         //是否结束游戏
         s = 0;
         //黑棋先
+        cont = 0;
         both_sides.style.color = "black";
         both_sides.innerHTML = "黑棋先..";
         //清除棋子
